b6790ef2f904f321f7bd9829dd131d3a050ae4f1
Tejaswini2609 <tejaswinid2004@gmail.com>
2025-10-31 20:16:03 +0530

    Your commit message here

--- dockploy.py
+++ dockploy.py
@@ -4,7 +4,7 @@ import requests
  
 API_URL =  "http://13.200.200.238:3000/api/project.all"
 api_key = "psWxvPUNiiXpmvwIWMmEJbXMkrubcPbJyDdGHZDVmusuvWAdxWRWyWkmXqCoTdfZ" 
-app_name = "job-portal-backend-brvs1x" 
+app_name = "job-portal-job-portal-frontend-lptih3"  
  
 def get_application_id(): 
    """Fetch Application ID""" 
